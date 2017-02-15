200b8f9c65be44806b9f47ca036ab38d6a52e1c3
Belen Curcio <curciobelen@gmail.com>
2017-02-15 10:06:18 -0300

    todo form

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,90 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+// import 'whatwg-fetch'
 
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      item: '',
+      alert: false
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      item: e.currentTarget.value
+    })
+  }
+
+  hideAlert = () => {
+    this.setState({
+      alert: false
+    })
+  }
+
+  showAlert = (e) => {
+    this.setState({
+      alert: true
+    }, () => {
+      window.setTimeout(this.hideAlert, 2000)
+    });
+  }
+
+  handleClick = (e) => {
+    fetch('/todo', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        item: this.state.item
+      })
+    }).then(response => {
+      return response.json()
+    }).then((json) => {
+      return this.showAlert()
+    }).catch(ex => {
+      console.log('Error: ', ex)
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <nav className="navbar navbar-inverse">
+          <div className="container">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="#">List</a>
+            </div>
+            <div id="navbar" className="collapse navbar-collapse">
+            </div>
+          </div>
+        </nav>
+
+        <div className="container">
+          <div className="jumbotron">
+            {this.state.alert ? <div className="alert alert-success" role="alert"> New todo added! </div> : null}
+            <h3>Add Todos</h3>
+            <div className="input-group">
+              <input className="form-control" type="text" onChange={this.handleChange} placeholder="Buy stuff, pick up something ..."/>
+              <span className="input-group-btn">
+                <button className="btn btn-success" onClick={this.handleClick} type="button">Add</button>
+              </span>
+            </div>
+          </div>
+          <span>
+          Spreadsheet: <a href="https://docs.google.com/spreadsheets/d/1D3QXR8O_fCQebEa5OM6wPGPVgBy-Mi_RwR7fVKKTo9Q/edit#gid=0">https://docs.google.com/spreadsheets/d/1D3QXR8O_fCQebEa5OM6wPGPVgBy-Mi_RwR7fVKKTo9Q/edit#gid=0</a>
+          </span>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-app.use(bodyParser.json())
 
-const googleapis = require('googleapis')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const google = require('googleapis')
+const credentials = require('./credentials.json')
 
 const auth = new google.auth.JWT(
     credentials.client_email,
@@ -18,9 +27,7 @@ google.options({ auth })
 const sheets = google.sheets('v4')
 const spreadsheetId = '1D3QXR8O_fCQebEa5OM6wPGPVgBy-Mi_RwR7fVKKTo9Q'
 
-app.get('/', (req, res) => {
-    res.send('List')
-})
+app.use(express.static('client/build'))
 
 app.post('/todo', (req, res) => {
     sheets.spreadsheets.values.append({
@@ -29,7 +36,7 @@ app.post('/todo', (req, res) => {
         valueInputOption: 'USER_ENTERED',
         includeValuesInResponse: true,
         resource: {
-            values: [[req.body.todo]]
+            values: [[req.body.item]]
         }
     }, (err, response) => {
         res.send(response.updates)
